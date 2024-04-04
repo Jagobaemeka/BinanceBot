1c6ad5a5be44c09cb8343030d24fea65a7a215dd
Emeka <Jagobaadimora@gmail.com>
2024-04-04 11:02:11 +0100

    Fixed variable name from client to Client

--- Bbot.py
+++ Bbot.py
@@ -1,8 +1,12 @@
 from binance.client import Client 
+## from decouple import config 
 
 
-api_key = ''
-api_secret = ''
+api_key = 'ALsgaJ0YT0aLV0GrRLzE9V7zNMDGvpMJmfg1iTit6oHGibcebwi27FO6HoPAw6zi'
+api_secret = 'ZDQzixxr21pO1LYUt8ntCbZ2P5uL0PufBppGdN32R1vEtU8rMFkfUlv21HSAEr2m'
 
-Client = Client(api_key, api_secret testnet=True )
+Client = Client(api_key, api_secret, testnet=True )
 
+balance = Client.get_asset_balance(asset = "BTC")
+
+print(balance)
